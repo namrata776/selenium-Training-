ee37c7c405c62d9a884644299eebe9718b5ecef3
Lenovo <Lenovo@DESKTOP-5KF0IL0>
2024-10-23 13:41:43 +0530

    Page Object Model 2nd Commit

---
+++ newTestingMavenTool/src/test/java/Pages/LoginPage.java
@@ -0,0 +1,47 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	WebDriver driver;
+	
+	@FindBy(name="Username")
+	private WebElement usertextbox;
+	@FindBy(name="password")
+	private WebElement passfield;
+	@FindBy(xpath="//button[.=' Login ']")
+	private WebElement loginbutton;
+	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void enterusername(String user) {
+		
+		usertextbox.sendKeys(user);
+	}
+	
+	public void enterpassword(String pass) {
+		passfield.sendKeys(pass);
+	}
+	
+	public void clicklogin() {
+		loginbutton.click();
+	}
+	
+	public void logintoapp( String user,String pass) {
+		  
+	     this.enterusername(user);
+	     this.enterpassword(pass);
+	     this.clicklogin();
+	  
+ }
+
+}
